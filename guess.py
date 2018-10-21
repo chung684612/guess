7ba093fdb5ce9d720b47930a7559035d86309c93
John Chung <cckcck@gmail.com>
2018-10-21 23:21:38 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,7 +1,11 @@
 import random
 
+s = input('請輸入起始數字: ')
+e = input('請輸入結束數字: ')
+s = int(s)
+e = int(e)
 time = 0
-r = random.randint(1, 100)
+r = random.randint(s, e)
 while True:
 	num = input('請猜數字: ')
 	num = int(num)
