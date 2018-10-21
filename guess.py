de05cd70a3e84a9f2a37bf5f54b9a1380520e97f
John Chung <cckcck@gmail.com>
2018-10-21 23:09:11 +0800

    Add count times

--- guess.py
+++ guess.py
@@ -1,11 +1,13 @@
 import random
 
+time = 0
 r = random.randint(1, 100)
 while True:
 	num = input('請猜數字: ')
 	num = int(num)
+	time += 1
 	if num == r:
-		print('恭喜您答對了!')
+		print('恭喜您答對了!', '共猜了', time, '次')
 		break
 	elif num > r:
 		print('答錯了', num, '比較大')
